0b0d1f494f2f3803cef2a70a5a4485cd2df43438
tony-stone <tony-stone@users.noreply.github.com>
2020-01-13 17:02:00 +0000

    Package now run on basis that OHCAO data provided to it

--- r/descriptive_stats.r
+++ r/descriptive_stats.r
@@ -1,25 +1,30 @@
 #' Generate descriptive statistics
 #'
-#' @param ohcao_data_file Path to RDS files of prepared OHCAO
-#' data, a character vector of length one.
+#' @param data A data.table of prepared OHCAO data
 #' @param output_directory Path to desired output directory,
 #' a character vector of length one.
 #'
-#' @return Nothing. On success and html file named "Report.html"
-#' will be created in the the specified output directory.
+#' @return Nothing. On success and html file named
+#' "OHCAO_data_report.html" will be created in the
+#' specified output directory.
 #'
 #' @examples
-#' generateDescriptiveStats("path/to/data.rds", "path/to/output/dir/")
-generateDescriptiveStats <- function(ohcao_data_file, output_directory) {
-  # read in data
-  ohcao_data <- readRDS("data/ohcao_data_2020-01-08.rds")
-  #ohcao_data <- readRDS(ohcao_data_file)
+#' generateDescriptiveStats(ohcao_data, "path/to/output/dir/")
+generateDescriptiveStats <- function(data, output_directory) {
 
-  ggplot2::ggplot(ohcao_data, ggplot2::aes(x = ems_month, colour = site)) +
+  ggplot2::ggplot(data, ggplot2::aes(x = ems_month, colour = site)) +
     ggplot2::geom_line(stat = "count") +
     ggplot2::scale_color_brewer(palette = "Paired") +
     ggplot2::scale_x_date(date_labels = "%Y-%b", date_breaks = "6 months")
 
-  DataExplorer::create_report(ohcao_data)
+  ?DataExplorer::create_report(data,
+                               output_file = "OHCAO_data_report.html",
+                               output_dir = output_directory,
+                               config = DataExplorer::configure_report(add_plot_qq = FALSE,
+                                                                       add_plot_bar = FALSE,
+                                                                       add_plot_correlation = FALSE,
+                                                                       add_plot_prcomp = FALSE,
+                                                                       add_plot_boxplot = FALSE,
+                                                                       add_plot_scatterplot = FALSE))
 }
 
---
+++ r/prepare_ohcao_data.r
@@ -0,0 +1,91 @@
+#' Prepare OHCAO data for analysis
+#'
+#' @param ohcao_data_raw A data.table of raw OHCAO data.
+#'
+#' @return A data.table of prepared OHCAO data.
+#'
+#' @examples
+#' prepareOHCAOData(ohcao_data_raw)
+prepareOHCAOData <- function(ohcao_data_raw) {
+
+  # Attach coding labels to data --------------------------------------------
+
+  recodes <- coding_tables[["data_coding"]][substr(data_coding_type, 1, 2) == "tb" & field %in% colnames(ohcao_data_raw)]
+  recode_field_names <- recodes$field
+
+  for(var in recode_field_names) {
+    data.table::setnames(coding_tables[[recodes[field == var, data_coding_type]]], paste0(var, c("", "_original")))
+    ohcao_data_raw <- merge(ohcao_data_raw,
+                            coding_tables[[recodes[field == var, data_coding_type]]],
+                            by = var,
+                            all.x = TRUE)
+  }
+
+  # Replace original codes with labels
+  recode_field_name_labels <- paste0(recode_field_names, "_original")
+  ohcao_data_raw[, (recode_field_names) := .SD, .SDcols = recode_field_name_labels]
+
+
+  # Remove values for (effectively) missing coded data ----------------------
+
+  ohcao_data_raw[, (recode_field_names) := lapply(.SD, function(vec) {
+    replace(vec, vec %in% c("Not recorded", "Not applicable", "Unobtainable", "Unknown"), NA)
+  }), .SDcols = recode_field_names]
+
+
+
+  # standardise field names (& remove prefixes) -----------------------------
+
+  data.table::setnames(ohcao_data_raw, make.names(tolower(sub("^clg_", "", colnames(ohcao_data_raw))), unique = TRUE))
+
+
+  # Remove missing / non-hospital codes -------------------------------------
+  # [all ambiguous values have frequency at least double of any recognisably valid hospital]
+
+  ohcao_data_raw[, hospcode_original := hospcode]
+
+  ohcao_data_raw[, hospcode := trimws(gsub("[\\s]{1,}", " ", gsub("[^A-Z0-9 ]{1,}", "", toupper(hospcode_original)), perl = TRUE))]
+
+  non_hospital_codes <- toupper(c("", "NOT CONVEYED", "9009", "XXX", "NA", "0",
+                                  "NOT TRANSPORTED", "9008", "ROLE",
+                                  "ROLE ON SCENE", "PATIENT NOT CONVEYED",
+                                  "UNKNOWN", "NOT APPLICABLE", "PLEASE SELECT",
+                                  "OTHER", "UNKNOWN X X X", "OTHER HOSPITAL",
+                                  "MISCELLANEOUS HOSPITAL ENTER DETAILS IN NOTES",
+                                  "NOT RECORDED", "OUT OF AREA"))
+  ohcao_data_raw[hospcode %in% non_hospital_codes, hospcode := NA]
+
+  # Remove missing data from time fields and convert to decimal hour --------
+  # (midnight = 0)
+
+  time_fields <- c("emstime", "stoptime")
+  ohcao_data_raw[, (time_fields) := lapply(.SD, function(time) {
+    time <- replace(time, time == "", NA)
+    (as.numeric(lubridate::fast_strptime(paste("2014-01-01", time), format = "%Y-%m-%d %H:%M:%S", tz = "Europe/London", lt = FALSE)) -
+        as.numeric(lubridate::fast_strptime("2014-01-01 00:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "Europe/London", lt = FALSE))) / 3600
+  }), .SDcols = time_fields]
+
+
+
+  # standardise data types --------------------------------------------------
+
+  ohcao_data_raw[, ems_month := as.Date(lubridate::fast_strptime(paste(emsdate, "12:00:00"), format = "%Y%B %H:%M:%S", tz = "Europe/London", lt = FALSE))]
+
+
+  # Ensure OHCAO IDs are unique ---------------------------------------------
+
+  stopifnot(ohcao_data_raw[, .N, by = ohcaoid][N != 1, .N] == 0)
+
+
+  # Define analysis dataset -------------------------------------------------
+
+  ohcao_data <- data.table::copy(ohcao_data_raw[, .(ohcaoid, site, ems_month, emstime, responsetime,
+                                                    sex, age, imd2015decile, wit, cprlay, padused,
+                                                    initrhythm, roscpreems, role, hospcode, discharged,
+                                                    roschosp)])
+
+
+  # Return data -------------------------------------------------------------
+
+  return(ohcao_data)
+}
